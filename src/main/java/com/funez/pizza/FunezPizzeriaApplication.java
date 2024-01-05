6f66857928119640d5a3694dfac5cacd34cea657
Jose Antonio Funez Perez <46043946@comcel.com.co>
2024-01-04 20:25:59 -0500

    First Commit - Spring Initializr project

---
+++ src/main/java/com/funez/pizza/FunezPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.funez.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunezPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunezPizzeriaApplication.class, args);
+	}
+
+}
